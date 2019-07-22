8c98cfa5ccd7e6a8bc368e986b7f9768db0446b6
Kim Jun Sik <jinaganda5@gmail.com>
2019-07-22 11:39:12 +0900

    Introduction to useState

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
-import React, { useRef, useEffect, createRef } from "react";
-import ReactDOM from "react-dom";
-
-const clickOutside = ftn => {
-  const ref = createRef();
-  const handleClick = e => {
-    if (!ref.current.contains(e.target)) {
-      ftn();
-    }
-  };
-  useEffect(() => {
-    document.addEventListener("click", handleClick);
-  }, []);
-  return ref;
-};
+import React, { useState } from "react";
 
 const App = () => {
-  const onClickOutside = () => {
-    console.log("Out!");
-  };
-  const ref = clickOutside(onClickOutside);
+  const [item, setItem] = useState(0);
+  const IncrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
   return (
     <div className="App">
-      <div ref={ref}>
-        <h1>Hello Nicolas</h1>
-        <h2>This should be a popup</h2>
-      </div>
-      <input type="text" />
+      <h1>Hello ReactHooks! </h1>
+      <h2>{item}</h2>
+      <br />
+      <button onClick={IncrementItem}>Increment</button>
+      <button onClick={decrementItem}>Decrement</button>
     </div>
   );
 };
