8bfd40052c25e224f48dc7551be5ccc3dc02dbf2
Kim Jun Sik <jinaganda5@gmail.com>
2019-07-22 09:39:19 +0900

    useInput & useFetch with useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React, { Component, useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const App = () => {
-  const [potato, Pizza] = useState(0);
-  const [email, setEmail] = useState("");
-  const updateEmail = e => {
+const useInput = defaultValue => {
+  const [value, setValue] = useState(defaultValue);
+
+  const onChange = e => {
     const {
       target: { value }
     } = e;
-    setEmail(value);
+    setValue(value);
+  };
+  return { value, onChange };
+};
+
+const useFetch = url => {
+  const [payload, setPayload] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  const callUrl = async () => {
+    try {
+      const { data } = await axios.get(url);
+      setPayload(data);
+    } catch {
+      setError("Error!");
+    } finally {
+      setLoading(false);
+    }
   };
 
+  useEffect(() => {
+    callUrl();
+  }, []);
+
+  return { payload, loading, error };
+};
+
+const App = () => {
+  const name = useInput("");
+  const { payload, loading, error } = useFetch("https://aws.random.cat/meow");
+  console.log(name);
   return (
-    <>
-      <div>{potato}</div>
-      <button onClick={() => Pizza(potato + 1)}>Increment</button>
-      <button onClick={() => Pizza(potato - 1)}>Decrement</button>
-      <input
-        placeholder="Write your Email"
-        value={email}
-        onChange={updateEmail}
-      />
-    </>
+    <div className="App">
+      <h1>Use Hooks!</h1>
+      <br />
+      <input {...name} placeholder="What's your name?" />
+      <br />
+      {loading && <span>loading your cat!</span>}
+      {!loading && error && <span>{error}</span>}
+      {!loading && payload && <img src={payload.file} width="250" />}
+    </div>
   );
 };
 
