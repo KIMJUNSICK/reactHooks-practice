d624a4c304525474e3f91fdcf17f8e4880502fe2
Kim Jun Sik <jinaganda5@gmail.com>
2019-07-24 18:16:06 +0900

    useClick( )

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { useRef, useState, useEffect } from "react";
+import React from "react";
+import useClick from "./useClick";
 
 const App = () => {
-  const potato = useRef();
-  setTimeout(() => potato.current.focus(), 5000);
+  const onClick = () => console.log("clicked!");
+  const title = useClick(onClick);
   return (
     <div className="App">
-      <div>Hello, it's Me</div>
-      <input ref={potato} placeholder="lala" />
+      <h1 ref={title}>Hello</h1>
     </div>
   );
 };
---
+++ src/useClick.js
@@ -0,0 +1,20 @@
+import { useRef, useEffect } from "react";
+
+const useClick = onClick => {
+  const element = useRef();
+  useEffect(() => {
+    if (typeof onClick === "function") {
+      if (element.current) {
+        element.current.addEventListener("click", onClick);
+      }
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+  return element;
+};
+
+export default useClick;
